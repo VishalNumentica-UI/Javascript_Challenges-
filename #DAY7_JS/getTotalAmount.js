3bf85be01e22bf96cd69d1c376a73312bf5a2155
VishalNumentica-UI <vishal.gopi@numentica-ui.com>
2023-11-01 15:59:10 +0530

    #DAY7_JS

--- #DAY7_JS/getTotalAmount.js
+++ #DAY7_JS/getTotalAmount.js
@@ -1,39 +1,40 @@
-import myJson from './storeProductList.json';
+//Getting json data from the user
+import storeProductList from './storeProductList.json';
 
 function getTotalAmount(stockDetails,priceDetails){
-    let sum=0,quantityChange=0;
+    let sum=0;
+    let quantityChange=0;
+    
+    priceDetails.forEach(getPrice =>{
+        stockDetails.forEach(productItem => {
 
-    stockDetails.forEach(productItem => {
-
-        priceDetails.forEach(getPrice =>{
-
-           if(productItem.product === getPrice.item && (typeof productItem.product ==="string" && typeof getPrice.item === "string")){
-             
+           if((productItem.product === getPrice.item)){
+            
             if(getPrice.quantity > productItem.Quantity){
-                  quantityChange= productItem.Quantity;
-                  productItem.Quantity-=quantityChange;
+                quantityChange= productItem.Quantity;   
             }
             else{
-                quantityChange = getPrice.quantity;
-                productItem.Quantity-=quantityChange;
-                
+                quantityChange = getPrice.quantity;       
             }
-            let removeCharacter=productItem.price.replace(/[$]/g,'')
-            let value=parseFloat(removeCharacter);
+            productItem.Quantity-=quantityChange;
+            const removeCharacter=productItem.price.replace(/[$]/g,'')
+            const value=parseFloat(removeCharacter);
             sum+=value*quantityChange;
               
            }
-           else{
-               return "Invalid Arguments is passed";
-           }
+           
         })
+        
     });
+    
     return `$${sum.toFixed(2)}`;
 }
-const getPrice=[
+
+
+const purchaseDetails=[
     { item: 'Jam - Apricot', quantity:1 },
     { item: 'Creamers - 10%', quantity:1 },
     { item: 'Jam - Apricot', quantity:1}
   ]
 //Result
-console.log(getTotalAmount(myJson,getPrice));
+console.log(getTotalAmount(storeProductList,purchaseDetails));
